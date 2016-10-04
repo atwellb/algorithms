90437f10f18a0df9a4a80d55d6bbc57586452273
Blake Atwell <blake.h.atwell@gmail.com>
2016-10-04 18:29:47 -0400

    InsertionSort

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,22 @@
+package sorting;
+
+public class InsertionSort {
+
+	public static void sort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			int j = i;
+			while (j > 0) {
+				if (arr[j - 1] > arr[j]) {
+					swap(arr, j - 1, j);
+				}
+				j--;
+			}
+		}
+	}
+
+	private static void swap(int[] arr, int a, int b) {
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
+}
